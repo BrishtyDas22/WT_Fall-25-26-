7c2bb52693eacd0d9faf40c7652944f0f0b47138
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-11-06 12:18:01 +0600

    2nd program

---
+++ MID/CLASS WORK/2nd program.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>AIUB Registration</title>
+</head>
+<body>
+    <center>
+<h1>AIUB Pre Registration Programh</h1>
+
+</center>
+
+    Enter your name:
+    <input type ="text">
+    <br><br>
+     Enter your semester:
+    <input type ="number">
+    <br> <br>
+     Enter your completed credits:
+    <input type ="number">
+    <br> <br>
+     Enter your Required credit:
+    <input type ="number">
+    <br> <br>
+    
+    
+    Enter your course name:
+     <input type ="radio">bangla
+      Enter your section:
+    <input type ="text">
+    <br> <br>
+      <input type ="radio">english
+      <br> 
+      semester:
+  
+     <center>
+       <input type ="submit">
+       <br> <br>
+        <input type ="reset">
+         <br><br>
+          </center>
+          <br>
+          
+
+    <input type="button">
+        
+
+    
+</body>
+</html>
