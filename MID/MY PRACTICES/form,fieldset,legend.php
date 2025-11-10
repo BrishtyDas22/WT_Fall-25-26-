7065affb38d518fd96cf7463cc001fe50dcd87d4
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-11-11 03:41:07 +0600

    form,fiekdset,legend

---
+++ MID/MY PRACTICES/form,fieldset,legend.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>FORM,FIELDSET,TITLE</title>
+</head>
+<body>
+    <p> hello web tech </p>
+    <form>
+        <fieldset>
+            <legend>
+                info 1
+</legend>
+        enter your name:
+        <input type="text" >
+        <br>
+        <br>
+         enter your id:
+        <input type="number" >
+</fieldset>
+           <br>
+        <br>
+           <br>
+        <br>
+        <fieldset>
+                        <legend>
+                info 2
+</legend>
+         enter your cgpa:
+        <input type="number" >
+           <br>
+        <br>
+         enter your age:
+        <input type="number" >
+</fieldset>
+           <br>
+        <br>
+           <br>
+        <br>
+        <fieldset>
+                        <legend>
+             submit
+</legend>
+        <input type="submit" value="done">
+        </fieldset>
+</form>
+</body>
+</html>
+
