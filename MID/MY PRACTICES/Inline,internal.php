51e710b76680ad1ea5dbcbe018f1ebeb09218962
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-11-11 04:27:54 +0600

    css

---
+++ MID/MY PRACTICES/Inline,internal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Document</title>
+  <style>
+
+
+h2 {
+  color: orange;
+
+}
+</style>
+</head>
+<body>
+   <h1 style= "color:blue;">this is in line css</h1> 
+<h2 style="color:red;">this is internal css</h2>
+<h2>this is internal css</h2>
+<h2>this is internal css</h2>
+<h2>this is internal css</h2>
+
+</body>
+</html>
