c81075a1049ae11bdac13b290df4830f14f9b218
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-11-17 10:57:26 +0600

    lab task 2

--- MID/LAB TASK SUBMISSION/Lab Task 2.php
+++ MID/LAB TASK SUBMISSION/Lab Task 2.php
@@ -2,7 +2,27 @@
 <html>
 <head>
   <title>LAB TASK 2</title>
-  
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      padding: 30px;
+      background-color: #f0f8ff;
+    }
+ 
+    h2 {
+      text-align: center;
+      color: #003366;
+    }
+ 
+    form {
+      background-color: #ffffff;
+      padding: 20px;
+      border-radius: 10px;
+      width: 300px;
+      margin: 0 auto;
+      box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    }
+    </style>
 </head>
 <body>
  
@@ -30,7 +50,7 @@
  
      <button type="submit">Add Course</button>
  
-    
+    <input type="text" id="web tech" />
   </form>
  
   
