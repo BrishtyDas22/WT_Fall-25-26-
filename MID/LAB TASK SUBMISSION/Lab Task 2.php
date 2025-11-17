cc7f461b7bff713ccf33dfb66329a06268074e30
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-11-17 10:54:58 +0600

    lab task 2

---
+++ MID/LAB TASK SUBMISSION/Lab Task 2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LAB TASK 2</title>
+  
+</head>
+<body>
+ 
+  <h2>Student Registration</h2>
+ 
+  <form onsubmit="return handleSubmit()">
+    <label>Full Name:</label>
+    <input type="text" id="name" />
+ 
+    <label>Email:</label>
+    <input type="text" id="studentId" />
+ 
+    <label>Password:</label>
+    <input type="number" id="age" />
+ 
+    <label>confirm Password:</label>
+    <input type="number" id="age" />
+  
+ 
+    <button type="submit">Register</button>
+  </form>
+  <form onsubmit="return handleSubmit()">
+    <label>Course Name:</label>
+    <input type="text" id="name1" />
+ 
+     <button type="submit">Add Course</button>
+ 
+    
+  </form>
+ 
+  
+</body>
+</html>
