fd6cd3a280734f4830f79674ed327d445e76f8f3
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-12-11 23:43:50 +0600

    practice2

---
+++ FINAL/my practices/php slide1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>php slide 1</title>
+</head>
+<body>
+    <?php
+    $txt1="php";
+    $txt2=" w3 schools";
+    echo "<h1>$txt1</h1>";
+    echo '<h2>'.$txt2.'</h2>';
+      echo '<p>welcome to '.$txt1.'  learing</p>';
+     echo "<p>you should study from $txt2</p>";
+     echo "i love ".$txt1." !";
+  
+
+    ?>
+</body>
+</html>
