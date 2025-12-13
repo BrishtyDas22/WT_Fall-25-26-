14ecc1f391eb9f683581148a9993bdfda7934b8c
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-12-14 03:02:05 +0600

    login html

---
+++ Project-Hostel Management System/Admin/HTML/Login_html.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html >
+<head>
+   
+    <title>Student Login</title>
+    <link rel="stylesheet" href ="../CSS/Login.css">
+</head>
+<body>
+    <h1> Student Login</h1>
+    <?php include '../PHP/login_validation.php';
+ ?>
+    <form method="post" action="">
+Student-ID:
+    <input type="number" name="id" value="<?php echo $id;?>" >
+    <?php echo $id_error;?>
+    <br><br>
+Name:
+    <input type="text" name="name" value="<?php echo $name;?>">
+    <?php echo $name_error;?>
+     <br><br>
+ Password:
+    <input type="password" name="password" value="<?php echo $password;?>">
+      <?php echo $password_error;?>
+       <br><br>
+      <input type="submit" name ="submit" value ="Login">
+       <br><br>
+</form>
+</body>
+</html>
