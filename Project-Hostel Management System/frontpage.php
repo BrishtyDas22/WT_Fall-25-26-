3ae4f28edc582d6dcecf8b63e7ef2bc8a1423ab6
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-12-20 17:23:11 +0600

    front

---
+++ Project-Hostel Management System/frontpage.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Happy Life</title>
+    <link rel="stylesheet" href="frontpagedesign.css">
+</head>
+<body>
+    <div id="header">
+        <img src="building-vector-icon-white-background_855620-52.avif" alt="Logo" id="logo">
+        <h1><b><i>Welcome to Happy-Life</i></b></h1>
+
+        <button type="button" id="register" onclick="handle()">Register Now</button>
+        <button type="submit" id="login">Login</button>
+
+    </div>
+    <br>
+
+    <div id="list">
+
+    <ul>
+    ⭐Our Facilities:
+        <li>Free-wifi</li>
+        <li>Generator</li>
+        <li>Cleanliness</li>
+        <li>Laundry service</li>
+        <li>24/7 security</li>
+        <li>Study room</li>
+        <li>3 times meals</li>
+        <li>Medical facility</li>
+    </ul>
+    </div>
+  <div id="room_box">
+
+
+    <img src="images/2bed.jpg" alt="room image ">
+
+    <div class="room_info"><h3>Room Info:</h3>
+        <p>Room No:101 </p>
+    <p>price: 10000tk/month</p>
+
+
+     </div> </div> 
+       <div id="room_box1">
+
+
+    <img src="images/2bed.jpg" alt="room image ">
+
+    <div class="room_info1"><h3>Room Info:</h3>
+        <p>Room No:102 </p>
+    <p>price: 10000tk/month</p>
+
+
+     </div> </div> 
+      <div id="room_box2">
+
+
+    <img src="images/2bed.jpg" alt="room image ">
+
+    <div class="room_info2"><h3>Room Info:</h3>
+        <p>Room No:103 </p>
+    <p>price: 10000tk/month</p>
+
+
+     </div> </div> 
+       
+</body>
+</html>
