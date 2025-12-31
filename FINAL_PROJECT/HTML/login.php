f19b60704639b43f045ff7a660839f350f999778
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2025-12-31 15:17:23 +0600

    1

---
+++ FINAL_PROJECT/HTML/login.php
@@ -0,0 +1,25 @@
+
+<?php include"../PHP/register_validation.php";?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Login</title>
+</head>
+<body>
+<h2>Admin Login</h2>
+
+<form method="post" action="">
+
+    Email:<br>
+    <input type="text" name="email"><br><br>
+
+    Password:<br>
+    <input type="password" name="password"><br><br>
+
+    <input type="checkbox" name="remember"> Remember Me<br><br>
+
+    <input type="submit" value="Login">
+
+</form>
+</body>
+</html>
