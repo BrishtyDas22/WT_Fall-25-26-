108d00f81ae9487b6750ca42dc28caabc7fdbe91
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2026-01-03 08:32:42 +0600

    34

---
+++ FINAL_PROJECT_1/Admin/VIEW/Admindashboard.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../CSS/admindashboard.css">
+</head>
+<body>
+    <div class="panel">
+
+    <h2>HMS Admin</h2>
+
+</div>
+<div class="sidebar">
+    <nav class="links"></nav>
+        <ul id="navlist">
+            <img src="../IMAGES/layout.png" alt="dasboardlogo" id="dashboardlogo">
+           <button type="submit" id="dashboard" onclick="window.location.href='Admindashboard.php'">Dashboard</button>
+
+           <img src="../IMAGES/management.png" alt="managestudentlogo" id="managestudentlogo">
+           <button type="submit" id="manageusers" onclick="window.location.href='ManageUsers.php'">Manage Users</button>
+
+           <img src="../IMAGES/engineer.png" alt="stafflogo" id="stafflogo">
+           <button type="submit" id="assignstaff" onclick="window.location.href='ManageHostels.php'">Assign staff</button>
+
+              <img src="../IMAGES/home-office.png" alt="roombookinglogo" id="roombookinglogo">
+           <button type="submit" id="managebookings" onclick="window.location.href='ManageBookings.php'">Room & Bookings</button>
+
+           <img src="../IMAGES/complaint.png" alt="complaintslogo" id="complaintslogo">
+           <button type="submit" id="complaints" onclick="window.location.href='ManageComplaints.php'">Complaints</button>
+
+              <img src="../IMAGES/bell.png" alt="noticeslogo" id="noticeslogo">
+           <button type="submit" id="notices" onclick="window.location.href='ManageNotices.php'">Notices</button>
+
+
+                <img src="../IMAGES/review.png" alt="studentfeedbacklogo" id="studentfeedbacklogo">
+           <button type="submit" id="Student-feedback" onclick="window.location.href='ManageFeedback.php'">Student Feedback</button>
+
+              <img src="../IMAGES/exit.png" alt="logoutlogo" id="logoutlogo">
+              <button type="submit" id="logout" onclick="window.location.href='Login.php'">Logout</button>
+        </ul>
+    </nav>
+
+    </div>
+    <div class="content">
+        <h1>Welcome to the Admin Dashboard</h1>
+
+</div>
+
+
+<div id="insidecontent">
+    <form method="post" id="roomcounts">
+        <Label id="roomnum">6</Label>
+        <br>
+        <label id="infoofroom">Total Rooms</label>
+        <img src="../IMAGES/bed.png" alt="bedlogo" id="bedlogo">
+        </form>
+
+
+
+        <form method="post" id="studentcounts">
+        <!-- Students number ekhane bosbe through table from database -->
+
+
+        <label id="infoofstudents">Total Students</label>
+        <img src="../IMAGES/teamwork.png" alt="userslogo" id="userslogo">
+
+    </form>
+
+    <form method="post" id="pendingcomplaints">
+        <!-- Pending complaints number ekhane bosbe through table from database -->
+
+        <label id="infoofcomplaints">Pending Complaints</label>
+        <img src="../IMAGES/project-deadline.png" alt="complaintlogo" id="complaintlogo">
+    </form>
+
+    
+    <form method="post" id="revenue">
+        <!-- Pending complaints number ekhane bosbe through table from database -->
+
+        <label id="infoofrevenue">Revenue</label>
+        <img src="../IMAGES/money.png" alt="revenuelogo" id="revenuelogo">
+    </form>
+
+
+
+
+</div>
+<div id="recentbookingtable">
+    <form action="" method="post">
+        <h2 id="recentbookingsheading">Recent Bookings</h2>
+        
+
+    </form>
+
+
+
+</div>
+    
+</body>
+</html>
